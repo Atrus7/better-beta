027a2485e6365bde45d7a2eeed1ef06daeb69b91
Atrus7 <cfindeisen7@gmail.com>
2014-08-30 16:19:04 -0500

    TogetherJS fixed effup

---
+++ BBeta.php
@@ -0,0 +1,37 @@
+<?php ?>
+
+<!DOCTYPE HTML>
+<html>
+<p>  Watching your every move </p>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="togetherjs1/togetherjs.js"></script>
+<script>
+        $('document').ready(function() {
+            TogetherJS();
+            TogetherJS();
+        });
+</script>
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
